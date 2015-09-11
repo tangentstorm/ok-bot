e004f50a08125e0110b664f7a2711562c360f1c3
tangentstorm <michal.wallace@gmail.com>
2015-09-11 12:00:00 +0000

    initial version

---
+++ ok-bot.js
@@ -0,0 +1,31 @@
+// An IRC bot for the k5 programming language,
+// using oK from : https://github.com/JohnEarnest/ok
+"use strict";
+var irc = require('irc');
+var ok = require('./ok/ok');
+
+const MAXLINES = 8;
+
+var client = new irc.Client('irc.freenode.net', 'ok-bot', {
+  channels: ['#jsoftware', '#learnprogramming', '#kq']
+});
+
+function runK(src) {
+  return ok.format(ok.run(ok.parse(src), ok.baseEnv()));
+}
+
+client.addListener('message', (from, to, msg)=> {
+  if (to.startsWith('#') && msg.startsWith('k ')) {
+    var result;
+    try { result = runK(msg.substring(2)); }
+    catch (err) { result = 'ERROR: ' + err.message; }
+    var line, lines = result.split("\n");
+    for (var i=0; i<lines.length; i++) {
+      if (i >= MAXLINES && lines.length != MAXLINES) {
+        line = '...';
+        break;
+    } else line = lines[i];
+      client.say(to, `${from}: ${line}`);
+    }
+  }
+});
