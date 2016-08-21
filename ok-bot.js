db60186f4affcc25760ae836eef7e57e4c34ce73
SquishyStrawberry <SquishyStrawberry@users.noreply.github.com>
2016-08-21 23:50:32 +0200

    Use Promises and add .gitignore

--- ok-bot.js
+++ ok-bot.js
@@ -11,10 +11,13 @@ const MAX_OUTPUT_LINES = 8;
 const env = ok.baseEnv();
 
 const runK = (src) => {
-  return ok.format(ok.run(ok.parse(src), env));
+  return new Promise((resolve, reject) => {
+    resolve(ok.format(ok.run(ok.parse(src), env)));
+  });
 };
 
-fs.readFile("config.json", (raw_data) => {
+fs.readFile("./config.json", (error, raw_data) => {
+  if (error) throw error;
   const json_data = JSON.parse(raw_data);
   const client = new irc.Client(json_data.server, json_data.nickname, {
     channels: json_data.channels
@@ -22,18 +25,19 @@ fs.readFile("config.json", (raw_data) => {
 
   client.addListener("message", (from, to, msg) => {
     if (to.startsWith("#") && msg.startsWith("k) ")) {
-      let result;
-      try {
-        result = runK(msg.substring(2));
-      } catch (err) {
-        result = `ERROR: ${err.message}`;
-      }
-      const lines = result.split("\n");
-      const line_amount = Math.min(lines.length, MAX_OUTPUT_LINES);
-      for (let i = 0; i < line_amount; ++i) {
-        client.say(to, `${from}: ${lines[i]}`);
-      }
-      if (lines.length > MAX_OUTPUT_LINES) client.say(to, `${from}: ...`);
+      runK(msg.substring(2)).then(
+        (result) => {
+          const lines = result.split("\n");
+          const line_amount = Math.min(lines.length, MAX_OUTPUT_LINES);
+          for (let i = 0; i < line_amount; ++i) {
+            client.say(to, `${from}: ${lines[i]}`);
+          }
+          if (lines.length > MAX_OUTPUT_LINES) client.say(to, `${from}: ...`);
+        },
+        (err) => {
+          client.say(to, `ERROR: ${err.message}`);
+        }
+      );
     }
   });
 });
