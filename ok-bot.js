8dff5b3268d0c40e5956c4f01d8afccde0b18ff9
SquishyStrawberry <SquishyStrawberry@users.noreply.github.com>
2016-08-21 23:08:54 +0200

    Refactor code and use config over hardcoding.

--- ok-bot.js
+++ ok-bot.js
@@ -1,34 +1,39 @@
+/* jshint esnext: true, node: true */
 // An IRC bot for the k5 programming language,
 // using oK from : https://github.com/JohnEarnest/ok
 "use strict";
-var irc = require('irc');
-var ok = require('./lib/ok/oK');
 
-const MAXLINES = 8;
+const fs = require("fs");
+const irc = require('irc');
+const ok = require('./lib/ok/oK');
 
-var client = new irc.Client('irc.freenode.net', 'ok-bot', {
-  channels: ['#jsoftware', '#learnprogramming', '#kq', '#lpmc']
-});
+const MAX_OUTPUT_LINES = 8;
+const env = ok.baseEnv();
 
-var env = ok.baseEnv();
-
-function runK(src) {
+const runK = (src) => {
   return ok.format(ok.run(ok.parse(src), env));
-}
+};
 
+fs.readFile("config.json", (raw_data) => {
+  const json_data = JSON.parse(raw_data);
+  const client = new irc.Client(json_data.server, json_data.nickname, {
+    channels: json_data.channels
+  });
 
-client.addListener('message', (from, to, msg)=> {
-  if (to.startsWith('#') && msg.startsWith('k) ')) {
-    var result;
-    try { result = runK(msg.substring(2)); }
-    catch (err) { result = 'ERROR: ' + err.message; }
-    var line, lines = result.split("\n");
-    for (var i=0; i<lines.length; i++) {
-      if (i >= MAXLINES && lines.length != MAXLINES) {
-        line = '...';
-        break;
-    } else line = lines[i];
-      client.say(to, `${from}: ${line}`);
+  client.addListener("message", (from, to, msg) => {
+    if (to.startsWith("#") && msg.startsWith("k) ")) {
+      let result;
+      try {
+        result = runK(msg.substring(2));
+      } catch (err) {
+        result = `ERROR: ${err.message}`;
+      }
+      const lines = result.split("\n");
+      const line_amount = Math.min(lines.length, MAX_OUTPUT_LINES);
+      for (let i = 0; i < line_amount; ++i) {
+        client.say(to, `${from}: ${lines[i]}`);
+      }
+      if (lines.length > MAX_OUTPUT_LINES) client.say(to, `${from}: ...`);
     }
-  }
+  });
 });
